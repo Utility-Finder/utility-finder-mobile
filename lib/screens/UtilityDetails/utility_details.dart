e1920def2a26f2e601f1e7da14e8fedea7e9fd1a
jasmaa <jasonmaa3955@gmail.com>
2021-03-14 17:00:47 -0400

    Add utility rating

--- lib/screens/UtilityDetails/utility_details.dart
+++ lib/screens/UtilityDetails/utility_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:utility_finder/models/utility.dart';
 import 'package:utility_finder/services/api.dart';
 
@@ -28,8 +29,54 @@ class _UtilityDetailsScreen extends State<UtilityDetailsScreen> {
                   placeholder: (context, url) => CircularProgressIndicator(),
                   errorWidget: (context, url, error) => Icon(Icons.error),
                 ),
-                Text(utility.rating.toString()),
-                Text(utility.description),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8),
+                  child: Column(children: [
+                    Text(
+                      utility.rating.toString(),
+                      style: TextStyle(fontSize: 50),
+                    ),
+                    Text(
+                      'out of 5',
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ]),
+                ),
+                RatingBar.builder(
+                  initialRating: 0,
+                  minRating: 0,
+                  direction: Axis.horizontal,
+                  allowHalfRating: false,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) async {
+                    // Send rating
+                    // TODO: cache rating
+                    _rating = rating.toInt();
+                    await rateUtility(utility, _rating);
+                    return showDialog(
+                      context: context,
+                      builder: (ctx) => AlertDialog(
+                        content: Text(
+                          "Thanks for your feedback!",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(ctx).pop();
+                            },
+                            child: Text("OK"),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ],
             ),
           ),
